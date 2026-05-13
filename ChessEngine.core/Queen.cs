c30886c421701250544622f2bf1de1589abab70d
Gevaudaan <agustin.aurellana278@gmail.com>
2026-05-13 18:31:25 -0300

    Se agrego la validacion de los movimientos del Rey y de la Reina

---
+++ ChessEngine.core/Queen.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChessEngineCore
+{
+    internal class Queen:Piece
+    {
+
+        public Queen(PieceColor color) : base(color)
+        {
+
+        }
+
+        public override bool IsValidMove(Position destination)
+        {
+            int moveDistanceX = destination.X - CurrentPosition.X;
+            int moveDistanceY = destination.Y - CurrentPosition.Y;
+
+            if (!FuncionesUtiles.OutOfBoard(destination))
+            {
+                return false;
+            }
+
+            if (moveDistanceX == moveDistanceY)
+            {
+                return true;
+            }
+            if (moveDistanceX != 0 && moveDistanceY != 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+
+
+        }
+
+    }
+}
